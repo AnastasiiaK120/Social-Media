5618996ee4a6c69beab54544be76e95696f3a661
AnastasiiaK120 <anastasiiakudriavtseva4@gmail.com>
2022-04-29 21:17:44 -0400

    Count and Display friends

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -21,6 +21,7 @@ from .views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home-view'),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
